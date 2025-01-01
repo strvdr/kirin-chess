0502cf230b880d0702ba7a8bde0f34f02b97c840
StrydrS <38505104+StrydrS@users.noreply.github.com>
2025-01-01 07:06:19 -0700

    XOR32Shift algo working as expected

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,35 @@ const utils = @import("utils.zig");
 const bitboard = @import("bitboard.zig");
 const attacks = @import("attacks.zig");
 
+fn getRandomNumber() u32 {
+    var number: u32 = bitboard.state;
+
+    number ^= number << 13;
+    number ^= number >> 17;
+    number ^= number << 5;
+
+    bitboard.state = number;
+
+    return number;
+}
+
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
 
     attacks.initLeaperAttacks();
-    const bitsInMask = utils.countBits(attacks.maskRookAttacks(@intFromEnum(bitboard.boardSquares.a1)));
-    std.debug.print("Bits in mask: {d}\n", .{bitsInMask});
-    const occupancy = utils.setOccupancy(4095, bitsInMask, attacks.maskRookAttacks(@intFromEnum(bitboard.boardSquares.a1)));
+    var randomNumber = getRandomNumber();
+    std.debug.print("Rand Num: {d}\n", .{randomNumber});
+    randomNumber = getRandomNumber();
+    std.debug.print("Rand Num: {d}\n", .{randomNumber});
+    randomNumber = getRandomNumber();
+    std.debug.print("Rand Num: {d}\n", .{randomNumber});
 
-    utils.printBitboard(occupancy);
+    // const bitsInMask = utils.countBits(attacks.maskRookAttacks(@intFromEnum(bitboard.boardSquares.a1)));
+    // std.debug.print("Bits in mask: {d}\n", .{bitsInMask});
+    // const occupancy = utils.setOccupancy(4095, bitsInMask, attacks.maskRookAttacks(@intFromEnum(bitboard.boardSquares.a1)));
+
+    // utils.printBitboard(occupancy);
 
     try bw.flush(); // Don't forget to flush!
 }
