f76e2bf6040109c796905fc9cd2cca18c2af14d3
StrydrS <38505104+StrydrS@users.noreply.github.com>
2025-01-01 10:38:51 -0700

    working on magic numbers

--- src/magics.zig
+++ src/magics.zig
@@ -15,7 +15,7 @@
 
 const bitboard = @import("bitboard.zig");
 
-pub fn getRandomNumberU32() u32 {
+fn getRandomNumberU32() u32 {
     var number: u32 = bitboard.state;
 
     number ^= number << 13;
@@ -28,7 +28,7 @@ pub fn getRandomNumberU32() u32 {
 }
 
 //from Tord Ramstad's Article on Generating Magic Numbers
-pub fn getRandomNumberU64() u64 {
+fn getRandomNumberU64() u64 {
     const n1: u64 = @as(u64, getRandomNumberU32()) & 0xFFFF;
     const n2: u64 = @as(u64, getRandomNumberU32()) & 0xFFFF;
     const n3: u64 = @as(u64, getRandomNumberU32()) & 0xFFFF;
@@ -37,6 +37,13 @@ pub fn getRandomNumberU64() u64 {
     return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
 }
 
-pub fn generateMagicNumber() u64 {
+fn generateMagicNumber() u64 {
     return getRandomNumberU64() & getRandomNumberU64() & getRandomNumberU64();
+}
+
+pub fn findMagicNumber(square: u6, relevantBits: u6, attackMask: u64) u64 { 
+    var occupancies: u64[4096] = undefined;
+    var attacks: u64[4096] = undefined;
+    var usedAttacks: u64[4096] = undefined;
+
 }
